35cf7045018622a713b4cf19ce3b8129b6eff5ac
Marek Linka <marek.linka@outlook.com>
2019-12-16 18:40:27 +0800

    Optimize performance and add benchmark for day 16

--- 16/UnitTest1.cs
+++ 16/UnitTest1.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using Xunit;
 
 namespace _16
 {
     public class UnitTest1
     {
+        [Fact]
+        public void Benchmark()
+        {
+            BenchmarkRunner.Run(GetType());
+        }
+
         [Fact]
         public void Part1()
         {
@@ -25,10 +34,9 @@ namespace _16
         public void Part2()
         {
             var text = File.ReadAllText("input.txt");
-            var input = text.Trim().ToCharArray().Select(_ => int.Parse(_.ToString())).ToList();
-
+            var input = text.Trim().ToCharArray().Select(_ => int.Parse(_.ToString())).ToArray();
             var offset = int.Parse(text.Substring(0, 7));
-            input = Repeat10K(input).Skip(offset).ToList();
+            input = Repeat10K(input).Skip(offset).ToArray();
 
             for (var i = 1; i <= 100; ++i)
             {
@@ -61,9 +69,9 @@ namespace _16
         [InlineData("03081770884921959731165446850517", 100, "53553731")]
         public void TestPart2(string inputString, int phases, string expected)
         {
-            var input = inputString.ToCharArray().Select(_ => int.Parse(_.ToString())).ToList();
+            var input = inputString.ToCharArray().Select(_ => int.Parse(_.ToString())).ToArray();
             var offset = int.Parse(inputString.Substring(0, 7));
-            input = Repeat10K(input).Skip(offset).ToList();
+            input = Repeat10K(input).Skip(offset).ToArray();
 
             for (var i = 1; i <= phases; ++i)
             {
@@ -73,7 +81,42 @@ namespace _16
             Assert.Equal(expected, string.Join(string.Empty, input.Take(8)));
         }
 
-        private static List<int> PhaseBack(List<int> input)
+        public int[] Input { get; set; }
+
+        public int Offset { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var text = File.ReadAllText("input.txt");
+            Input = text.Trim().ToCharArray().Select(_ => int.Parse(_.ToString())).ToArray();
+            Offset = int.Parse(text.Substring(0, 7));
+            Input = Repeat10K(Input).Skip(Offset).ToArray();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Part2Benchmark()
+        {
+            var input = Input;
+            for (var i = 1; i <= 100; ++i)
+            {
+                input = PhaseBack(input);
+            }
+        }
+
+        [Benchmark]
+        public void Part2VectorBenchmark()
+        {
+            var input = Input;
+            for (var i = 1; i <= 100; ++i)
+            {
+                input = PhaseBackVector(input);
+            }
+
+            Assert.Equal("80284420", string.Join(string.Empty, input.Take(8)));
+        }
+
+        private static int[] PhaseBack(int[] input)
         {
             // the trick here is that we offset is in the second half of the input
             // for the elements in the second half, the next value is only dependent on the elements in the second half again
@@ -84,13 +127,50 @@ namespace _16
             // then subtract the offset+1 element, that's what the offset+2 element depends on etc.
             // O(N) for N = TotalLength - offset
 
-            var list = new List<int>();
+            var list = new int[input.Length];
 
             var sum = input.Sum();
 
-            foreach (var i in input)
+            for (var index = 0; index < input.Length; index++)
             {
-                list.Add( Math.Abs(sum) % 10);
+                var i = input[index];
+                list[index] = Math.Abs(sum) % 10;
+                sum -= i;
+            }
+
+            return list;
+        }
+
+        private static int[] PhaseBackVector(int[] input)
+        {
+            // the trick here is that we offset is in the second half of the input
+            // for the elements in the second half, the next value is only dependent on the elements in the second half again
+            // if you look at the matrix calculation example, the first element in the second half is dependent on all elements in the second half
+            // the half+1 element is only dependent on sum of half+1 to the end
+            // the half+2 element is dependent on sum of half+2 to the end etc (it's an upper triangular matrix with only 1s)
+            // therefore we do the sum from the offset+1 to the end, that's what offset+1 element depends on
+            // then subtract the offset+1 element, that's what the offset+2 element depends on etc.
+            // O(N) for N = TotalLength - offset
+
+            var list = new int[input.Length];
+
+            var sumVector = new Vector<int>(0);
+
+            var subVectors = input.Length / Vector<int>.Count;
+
+            for (var i = 0; i < subVectors; ++i)
+            {
+                sumVector += new Vector<int>(input.AsSpan(Vector<int>.Count * i));
+            }
+
+            var remainder = input.Skip(Vector<int>.Count * subVectors).Sum();
+
+            var sum = Vector.Dot(sumVector, Vector<int>.One) + remainder;
+
+            for (var index = 0; index < input.Length; index++)
+            {
+                var i = input[index];
+                list[index] = Math.Abs(sum) % 10;
                 sum -= i;
             }
 
@@ -132,31 +212,6 @@ namespace _16
                     for (var j = 0; j < phaseNumber; ++j)
                     {
                         yield return i;
-                    }
-                }
-            }
-        }
-
-        private IEnumerable<int> GetPatternForPhase(List<int> patternSource, int phaseNumber, int offset)
-        {
-            var current = 0;
-            while (true)
-            {
-                foreach (var i in patternSource)
-                {
-                    for (var j = 0; j < phaseNumber; ++j)
-                    {
-                        if (current < offset)
-                        {
-                            current++;
-                        }
-                        else
-                        {
-                            current++;
-
-                            yield return i;
-
-                        }
                     }
                 }
             }
