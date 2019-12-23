86e31ed8c83dc398d5bf81d5b922e98c9eb74422
Marek Linka <marek.linka@outlook.com>
2019-12-23 17:00:23 +0800

    Reformat and add docs for day 4

--- fsharp/04/Tests.fs
+++ fsharp/04/Tests.fs
@@ -2,56 +2,82 @@ module Tests
 
 open Xunit
 
+
+/// **Description**
+/// Checks if the provided number is composed of non-decreasing digits (left to right)
+///
+/// **Parameters**
+///   * `number` - the number to check
+///
+/// **Output Type**
+///   * `bool` - `true` if the number has non-decrfeasing digits, otherwise false
 let isIncreasing number =
-  let (_, _, nonDecreasing) =
-    [ 5 .. -1 .. 0 ]
-    |> Seq.fold (fun (n, previousDigit, nonDecreasing) current ->
-      match nonDecreasing with
-      | false -> (n, previousDigit, false)
-      | true ->
-        let currentDigit = n / (pown 10 current)
-        let nextNumber = n - currentDigit * (pown 10 current)
+    let (_, _, nonDecreasing) =
+        [ 5 .. -1 .. 0 ]
+        |> Seq.fold (fun (n, previousDigit, nonDecreasing) current ->
+            match nonDecreasing with
+            | false -> (n, previousDigit, false)
+            | true ->
+                let currentDigit = n / (pown 10 current)
+                let nextNumber = n - currentDigit * (pown 10 current)
 
-        match (currentDigit < previousDigit) with
-        | true -> (nextNumber, currentDigit, false)
-        | false -> (nextNumber, currentDigit, true)) (number, 0, true)
+                match (currentDigit < previousDigit) with
+                | true -> (nextNumber, currentDigit, false)
+                | false -> (nextNumber, currentDigit, true)) (number, 0, true)
 
-  nonDecreasing
+    nonDecreasing
 
-  
+
+
+/// **Description**
+/// Checks if the provided number contains any digit twice in a row. E.g. 1123456 or 123455
+///
+/// **Parameters**
+///   * `number` - the number to check
+///
+/// **Output Type**
+///   * `bool` - `true` if the number contains any digit twice in a row, otherwise false
 let hasPair number =
-  let (_, _, hasPair) =
-    [ 5 .. -1 .. 0 ]
-    |> Seq.fold (fun (n, previousDigit, hasPair) current ->
-      match hasPair with
-      | false ->
-        let currentDigit = n / (pown 10 current)
-        let nextNumber = n - currentDigit * (pown 10 current)
+    let (_, _, hasPair) =
+        [ 5 .. -1 .. 0 ]
+        |> Seq.fold (fun (n, previousDigit, hasPair) current ->
+            match hasPair with
+            | true -> (n, previousDigit, hasPair) // short circuit
+            | false ->
+                let currentDigit = n / (pown 10 current)
+                let nextNumber = n - currentDigit * (pown 10 current)
 
-        match (currentDigit = previousDigit) with
-        | true -> (nextNumber, currentDigit, true) 
-        | false -> (nextNumber, currentDigit, false)
-      | true -> (n, previousDigit, hasPair)) (number, 0, false)
-        
-  hasPair
+                match (currentDigit = previousDigit) with
+                | true -> (nextNumber, currentDigit, true)
+                | false -> (nextNumber, currentDigit, false)) (number, 0, false)
 
+    hasPair
+
+
+/// **Description**
+/// Checks if the provided number contains any digit exactly twice in a row
+///
+/// **Parameters**
+///   * `number` - the number to check
+///
+/// **Output Type**
+///   * `bool` - `true` if the number contains any digit exactly twice in a row, otherwise false
 let hasExactPair number =
-  let (_, _, runLength, hasPair) =
-    [ 5 .. -1 .. 0 ]
-    |> Seq.fold (fun (n, previousDigit, runLength, hasPair) current ->
-      let currentDigit = n / (pown 10 current)
-      let nextNumber = n - currentDigit * (pown 10 current)
+    let (_, _, runLength, hasPair) =
+        [ 5 .. -1 .. 0 ]
+        |> Seq.fold (fun (n, previousDigit, runLength, hasPair) current ->
+            let currentDigit = n / (pown 10 current)
+            let nextNumber = n - currentDigit * (pown 10 current)
 
-      match hasPair, currentDigit with
-      | true, _             -> (nextNumber, currentDigit, runLength, hasPair)
-      | false, pd when previousDigit = pd -> (nextNumber, currentDigit, runLength + 1, hasPair)
-      | false, _            ->
-        match runLength with
-        | 2 -> (nextNumber, currentDigit, 1, true)
-        | _ -> (nextNumber, currentDigit, 1, hasPair)
-      ) (number, 0, 1, false)
-        
-  hasPair || runLength = 2
+            match hasPair, currentDigit with
+            | true, _ -> (nextNumber, currentDigit, runLength, hasPair) // short circuit
+            | _, pd when previousDigit = pd -> (nextNumber, currentDigit, runLength + 1, hasPair) // lengthen run
+            | _, _ ->
+                match runLength with
+                | 2 -> (nextNumber, currentDigit, 1, true)
+                | _ -> (nextNumber, currentDigit, 1, hasPair)) (number, 0, 1, false)
+
+    hasPair || runLength = 2 // the 2-run might be the last pair of digits in the number
 
 [<Theory>]
 [<InlineData(123456, true)>]
@@ -97,4 +123,5 @@ let Part2() =
         |> Seq.filter (fun n -> isIncreasing n && hasExactPair n)
         |> Seq.length
 
-    Assert.Equal(609, l)  
+    Assert.Equal(609, l)
+ 
